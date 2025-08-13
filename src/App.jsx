8c573238947e2aaceeedafab70afecc8723b959a
Vaiebhav Sharma <vaibhavrocks4ever@gmail.com>
2025-08-13 22:25:30 +0530

    background chnager App usestate tutorial firt commit-- Need to improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
     <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 ml-28 bg-slate-500'>
         Change Theme
-        <div className=' bg-black text-slate-200 cursor-pointer rounded-2xl' onClick={chnageColor}>Black</div>
+        <div className=' bg-black text-slate-200 cursor-pointer rounded-2xl' onClick={()=>setcolor('Black')}>Black</div>
         <div className='text-slate-200 bg-[#5F9EA0] cursor-pointer rounded-2xl' onClick={chnageColor}>Blue</div>
         <div className='bg-green-600 text-slate-200 cursor-pointer rounded-2xl' onClick={chnageColor}>Green</div>
         <div className='bg-[#F4A460] text-slate-200 cursor-pointer rounded-2xl' onClick={chnageColor}>Brown</div>
